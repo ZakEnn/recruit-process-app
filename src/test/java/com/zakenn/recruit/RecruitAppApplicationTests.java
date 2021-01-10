07047fb185bf80ade1ee3483f4c3aa6fc2b43ee0
zakenn <ennaje.zakari@gmail.com>
2021-01-10 23:58:08 +0100

    init

---
+++ src/test/java/com/zakenn/recruit/RecruitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakenn.recruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
